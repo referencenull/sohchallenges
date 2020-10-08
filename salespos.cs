825722d4c30c30ee443c69130f7b66d78a7d6fa8
Juha Karvonen <jukarvon@microsoft.com>
2020-10-08 14:03:46 +0300

    possales function

---
+++ salespos.cs
@@ -0,0 +1,15 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Extensions.Logging;
+
+namespace My.Functions
+{
+    public static class POSSales
+    {
+        [FunctionName("POSSales")]
+        public static void Run([EventHubTrigger("sohchallengeeventhub", Connection = "Endpoint=sb://sohchallenge.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=hx/dblFAlO5bg6IdeBe07bAraDP1ouZPfbjszyzezyU=")] string myEventHubMessage, ILogger log)
+        {
+            log.LogInformation($"C# function triggered to process a message: {myEventHubMessage}");
+        }
+    }
+}
